d0b6ca6d87ddc227feacab68a72bc98122619941
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-03-06 03:15:11 +0200

    Remove Text section

--- app/src/main/java/com/example/numberguessinggame/GameScreen.kt
+++ app/src/main/java/com/example/numberguessinggame/GameScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.material3.MaterialTheme.shapes
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
